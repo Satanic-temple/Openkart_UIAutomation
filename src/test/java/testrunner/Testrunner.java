1a46137e99b1bc70d125610fe02a48a86e6be5c6
Undead Sorcerer <satikghadei123@gmail.com>
2025-01-16 16:56:15 +0530

    demo1

--- src/test/java/testrunner/Testrunner.java
+++ src/test/java/testrunner/Testrunner.java
@@ -8,7 +8,8 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/features", glue = { "stepdefinitions", "AppHooks" }, plugin = {
-		"pretty", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" })
+		"pretty", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:", 
+		"json: target/cucumberjenkinsreport/cucumberreport.json"})
 public class Testrunner {
 
 }
